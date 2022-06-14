9e742b686a472f01d9c05b550e20c0f193d675f8
tnghung <tnghung2812@gmail.com>
2022-06-14 16:23:37 +0700

    Finish control section for mobile view

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const board = document.querySelector('.board-section');
 
-for (let i = 0; i < 150; i++) {
+for (let i = 0; i < 200; i++) {
   let block = '<div class="block"></div>';
   board.innerHTML += block;
 }
