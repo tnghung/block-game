4f7012df34a03d824e28b604ba3e7f37b197227d
tnghung <tnghung2812@gmail.com>
2022-06-15 12:49:02 +0700

    Add event click for button

---
+++ game.js
@@ -0,0 +1,37 @@
+const btns = document.querySelectorAll('[id*="btn-"]');
+
+btns.forEach((e) => {
+  const btn_id = e.getAttribute('id');
+  const body = document.querySelector('body');
+  e.addEventListener('click', () => {
+    switch (btn_id) {
+      case 'btn-play': {
+        body.classList.add('play');
+        if (body.classList.contains('pause')) {
+          body.classList.remove('pause');
+        }
+        break;
+      }
+      case 'btn-theme': {
+        body.classList.toggle('dark');
+        break;
+      }
+      case 'btn-help': {
+        body.querySelector('.how-to').classList.toggle('active');
+        break;
+      }
+      case 'btn-pause': {
+        const btn_play = body.querySelector('#btn-play');
+        btn_play.textContent = 'Resume';
+        body.classList.remove('play');
+        body.classList.add('pause');
+        break;
+      }
+      case 'btn-new-game': {
+        body.classList.add('play');
+        body.classList.remove('pause');
+        break;
+      }
+    }
+  });
+});
